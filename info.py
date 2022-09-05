56d5ef677f697d0d2693e70d0edb22321c69f2ac
Stark Industries TG <88097120+StarkIndustriesTG@users.noreply.github.com>
2022-09-05 10:15:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,15 +37,14 @@ class evamaria(Client):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+API_ID = int(1396584367)
+API_HASH = "ed8003bc110f89c314415faa79c953b4"
+BOT_TOKEN = "5259581724:AAGIMBK0k56iNWyLn-Y_SpBWZkUYys_iYFE"
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/7e56d907542396289fee4.jpg https://telegra.ph/file/9aa8dd372f4739fe02d85.jpg https://telegra.ph/file/adffc5ce502f5578e2806.jpg https://telegra.ph/file/6937b60bc2617597b92fd.jpg https://telegra.ph/file/09a7abaab340143f9c7e7.jpg https://telegra.ph/file/5a82c4a59bd04d415af1c.jpg https://telegra.ph/file/323986d3bd9c4c1b3cb26.jpg https://telegra.ph/file/b8a82dcb89fb296f92ca0.jpg https://telegra.ph/file/31adab039a85ed88e22b0.jpg https://telegra.ph/file/c0e0f4c3ed53ac8438f34.jpg https://telegra.ph/file/eede835fb3c37e07c9cee.jpg https://telegra.ph/file/e17d2d068f71a9867d554.jpg https://telegra.ph/file/8fb1ae7d995e8735a7c25.jpg https://telegra.ph/file/8fed19586b4aa019ec215.jpg https://telegra.ph/file/8e6c923abd6139083e1de.jpg https://telegra.ph/file/0049d801d29e83d68b001.jpg')).split()
-
+USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
+PICS = "https://telegra.ph/file/09d87bc3ad4736a038779.jpg"
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
@@ -70,22 +69,37 @@ TG_MAX_SELECT_LEN = environ.get("TG_MAX_SELECT_LEN", "100")
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Alexa:Alexa@cluster0.verfo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Alexa_Files')
 
 #Downloader
 DOWNLOAD_LOCATION = environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/AudioBoT/")
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TeamEvamaria')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1001702254960))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'heart_recipe')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "False")), False)
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", None)
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", """<code>{file_name}</code> 
+
+<b>[𝙹𝙾𝙸𝙽 𝚄𝚂 𝙵𝙾𝚁 𝙼𝙾𝚁𝙴 𝚄𝙿𝙳𝙰𝚃𝙴𝚂..](https://t.me/VK_LINKZ)</b>
+
+<b>🌟 𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 : @Vk_Botz</b>""")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>Query: {query}</b> \n‌IMDb Data:\n\n🏷 Title: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating}</a> / 10")
+IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", """<b>🎞 𝚃𝙸𝚃𝚃𝙻𝙴</b>: <a href={url}>{title}</a>
+<b>🎭 𝙶𝙴𝙽𝚁𝙴𝚂</b>: {genres}
+<b>📆 𝚈𝙴𝙰𝚁</b>: <a href={url}/releaseinfo>{year}</a>
+<b>🌟 𝚁𝙰𝚃𝙸𝙽𝙶</b>: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.)
+<b>🎙 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴𝚂</b> : <code>{languages}</code>
+<b>📀 𝚁𝚄𝙽𝚃𝙸𝙼𝙴</b>: {runtime} Minutes
+<b>📆 𝚁𝙴𝙻𝙴𝙰𝚂𝙴 𝙸𝙽𝙵𝙾</b> : {release_date}
+<b>🌀 𝙲𝙾𝚄𝙽𝚃𝚁𝙸𝙴𝚂</b> : <code>{countries}</code>
+
+<b>📚𝚁𝙴𝚀𝚄𝙴𝚂𝚃𝙴𝙳 𝙱𝚈 </b>: {message.from_user.mention}
+
+<b>♻️𝙵𝙾𝚁 𝙼𝙾𝚁𝙴 𝙹𝙾𝙸𝙽 𝚄𝚂</b>: @VK_LINKZ""")
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
