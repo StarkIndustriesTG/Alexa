824c3b4c9ace86dccbb9bb99e801a25c218740a3
PRAKASH <95958624+Heartrecipe@users.noreply.github.com>
2022-10-14 07:40:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -52,7 +52,7 @@ auth_users = [int(user) if id_pattern.search(user) else user for user in environ
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
 auth_grp = environ.get('AUTH_GROUP')
-AUTH_CHANNEL = int(-1001753307358)
+AUTH_CHANNEL = int(-1001634923998)
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 USE_AS_BOT = environ.get("USE_AS_BOT", True)
 
